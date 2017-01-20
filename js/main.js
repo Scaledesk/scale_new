bfaec52e144913bcca1ee6bb8d3775a47ef7a716
garima <garima8jun@gmail.com>
2017-01-20 18:50:00 +0530

    navigation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "50%";
+    document.getElementById("mySidenav").style.transition = "all .4s ease";
 }
 
 /* Set the width of the side navigation to 0 */
