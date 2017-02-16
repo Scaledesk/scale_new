61db81331f0380cd910c29b6c50d0c17809b88e5
garima <garima8jun@gmail.com>
2017-02-16 16:13:35 +0530

    contact us n testimonial section

--- js/main.js
+++ js/main.js
@@ -10,3 +10,23 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 /*//............ nav bar.............. // */
+
+
+
+
+/*
+scroll down button on footer*/
+
+$('.scroll-down').click(function(event) {
+    // Preventing default action of the event
+    event.preventDefault();
+    // Getting the height of the document
+    var n = $(document).height();
+    $('html, body').animate({ scrollTop: 650 }, 900);
+//                                       |    |
+//                                       |    --- duration (milliseconds)
+//                                       ---- distance from the top
+});
+
+
+/*scroll down button on banner end*/
