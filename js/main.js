a28eb2ab1eaf2c233d089ec26e50037d721f8a80
garima <garima8jun@gmail.com>
2017-02-17 14:50:31 +0530

    background from css

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 /*................. navbar..............  */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "100%";
+    document.getElementById("mySidenav").style.top = "0";
     document.getElementById("mySidenav").style.height = "15%";
     document.getElementById("mySidenav").style.transition = "all .4s ease";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("mySidenav").style.top = "-100px";
 }
 /*//............ nav bar.............. // */
 
